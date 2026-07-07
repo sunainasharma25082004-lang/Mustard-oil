a046b5f1e3188d023d6ff8316806293488388547
sunainasharma25082004-lang <sunainasharma25082004@gmail.com>
2026-07-07 16:00:22 +0530

    push

--- server/src/routes/shippingRoutes.js
+++ server/src/routes/shippingRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/config', getShippingConfig);
 router.get('/serviceability', getServiceability);
+router.post('/serviceability', getServiceability);
 
 module.exports = router;
